6e163cfa751adae080327522bc6c8ea0d5a0dee1
Ragul M <ragul.m@optisolbusiness.com>
2022-07-19 12:10:13 +0530

    First Commit.

---
+++ Tester/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Tester
+//
+//  Created by Mac-OBS-18 on 14/07/22.
+//
+
+import UIKit
+import SampleFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        SubFrameworkHome.sayHello()
+    
+        // Do any additional setup after loading the view.
+    }
+
+}
+
